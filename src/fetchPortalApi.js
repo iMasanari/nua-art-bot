d397289aed8925e592d36110317fcd251b8515db
iMasanari <14dd023@gmail.com>
2017-04-25 10:31:18 +0900

    情報取得用のトークンを、ログイン処理で動的に取得する方法に変更

--- src/fetchPortalApi.js
+++ src/fetchPortalApi.js
@@ -1,9 +1,25 @@
 const fetch = require('node-fetch')
-const bearer = process.env.NUA_BEARER
-const url = 'https://www.nua.ac.jp/portal/api/'
+const client = require('cheerio-httpcli')
+
+const UserName = process.env.NUA_USER_NAME
+const Password = process.env.NUA_PASSWORD
+
+const url = 'https://www.nua.ac.jp/portal'
+
+const tokenPromise = (async () => {
+  const loginPage = await client.fetch(`${url}/Account/Login?ReturnUrl=%2Fportal%2F`)
+  const loginRedirectPage = await loginPage.$('form').submit({ UserName, Password })
+
+  const _ClientTokenId = loginRedirectPage.body.match(/var _ClientTokenId = '(.+?)';/)[1]
+  const authorizePage = await client.fetch(`${url}/Account/Authorize?client_id=${_ClientTokenId}&response_type=token&state=`)
+
+  return authorizePage.response.cookies.tokenAuth_access_token
+})()
 
 module.exports = async (file) => {
-  const response = await fetch(url + file, { headers: { 'X-CPAuthorize': 'Bearer ' + bearer } })
+  const response = await fetch(`${url}/api/${file}`, {
+    headers: { 'X-CPAuthorize': `Bearer ${await tokenPromise}` }
+  })
 
   if (!response.ok) {
     throw `取得エラーが発生しました
